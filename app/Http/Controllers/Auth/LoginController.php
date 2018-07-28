b378fb26d2f7e1fa633cfeaec08f1306f9bd62fc
adis azhar <adisazhar123@gmail.com>
2018-07-29 03:58:47 +0700

    new

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,7 +41,7 @@ class LoginController extends Controller
     }
 
     public function showLoginForm(){
-      return view('login');
+      return view('index');
     }
 
     protected function authenticated(Request $request, $user){
